95619a07f9aa04449de8cb9b1e0e4ed1eda6f98e
Ailsasahda13 <ailsashda13@gmail.com>
2026-03-10 01:02:06 +0700

    menambahkan validasi input angka positif

--- TugasJobsheet3/tugasjobsheet3/bin/main.dart
+++ TugasJobsheet3/tugasjobsheet3/bin/main.dart
@@ -2,6 +2,9 @@ void main() {
   int angkaRahasia = 136;
   int tebakan = 150; 
 
+  if (tebakan <= 0) {
+    print("Error: Masukkan angka positif!");
+  }
   if (tebakan > angkaRahasia) {
     print("Yeayy, tebakanmu tepat nihh!!!");
   } else if (tebakan < angkaRahasia) {
